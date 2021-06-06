b719ad511ddf6c38a3bafab4d80de8a22deaec27
HATZ01 <noah.hatz01@htl-salzburg.ac.at>
2021-06-06 15:47:40 +0200

    Modify gitignore

---
+++ array/Assets/Scripts/Arrays.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] array1 = new string[4];
+        float[] array2 = new float[] { 21f, 22f, 23f, 24f };
+        int[] array3 = { 31, 32, 33, 34, 35, 36, 37 };
+    }
+
+    /* Aufgaben:
+    1) Durchlaufen ausgeben der Werte
+    2) Länge des Arrays ausgeben als Debug
+    3) 2 Werte direkt ändern
+    4) GameObjects Images anlegen, Tag vergeben, zuweisen und Farbe ändern.
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
